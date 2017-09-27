5013c070fd9f1f044d61ed9395b1bb84b98189d4
An-AngryBear <monahajt@gmail.com>
2017-09-27 13:08:06 -0500

    minor tweaks

--- app/models/Order.js
+++ app/models/Order.js
@@ -5,7 +5,8 @@ const { setActiveCustomer, getActiveCustomer } = require('../activeCustomer');
 const path = require('path');
 const dbPath = path.resolve(__dirname, '..','..', 'db','bangazon.sqlite');
 const db = new Database(dbPath);
-//if there is an open order, will resolve the ID of that order. If no order or if only closed orders will return -1
+
+//if there is an open order, will resolve the ID of that order. If no order or if only closed orders, will return -0
 let checkForOpenOrders = (id) => {
     return new Promise ( (resolve, reject) => {
         db.get(`SELECT orders.order_id, orders.payment_type, orders.buyer_id FROM orders 
@@ -13,7 +14,7 @@ let checkForOpenOrders = (id) => {
             `, (err, order) => {
                 if (err) return reject(err);
                 if(order && order.payment_type) {
-                    resolve(-1);
+                    resolve(-0);
                 } else {
                     resolve(order.order_id);
                 }
