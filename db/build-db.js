cd9e60866c3ec20e6bdd50af81f6ec70234fcb6d
cait-rocco <cait.rocco@gmail.com>
2017-09-27 15:42:31 -0500

    refactored build-db buildalltables function to individual tables

--- db/build-db.js
+++ db/build-db.js
@@ -11,67 +11,6 @@ let productTypesContent = JSON.parse(readFileSync("./data/productTypes.json"));
 let orderContent = JSON.parse(readFileSync("./data/orders.json"));
 let userContent = JSON.parse(readFileSync("./data/users.json"));
 
-//Resets the database -jmr
-let functionThatCreatesTables = () => {
-    return new Promise ( (resolve, reject) => {
-        db.serialize( () => {
-            db.run(`DROP TABLE IF EXISTS users`);
-            db.run(`DROP TABLE IF EXISTS paymentOptions`);
-            db.run(`DROP TABLE IF EXISTS orders`);
-            db.run(`DROP TABLE IF EXISTS products`);
-            db.run(`DROP TABLE IF EXISTS productOrders`);
-            db.run(`DROP TABLE IF EXISTS productTypes`);
-            db.run(`CREATE TABLE IF NOT EXISTS users(
-                user_id INTEGER PRIMARY KEY NOT NULL,
-                first_name TEXT NOT NULL,
-                last_name TEXT NOT NULL,
-                start_date TEXT NOT NULL,
-                street_address TEXT NOT NULL,
-                city TEXT NOT NULL,
-                state TEXT NOT NULL,
-                postal_code INTEGER NOT NULL,
-                phone TEXT NOT NULL,
-                email TEXT NOT NULL
-            )`);
-            db.run(`CREATE TABLE IF NOT EXISTS paymentOptions(
-                payment_id INTEGER PRIMARY KEY NOT NULL,
-                buyer_id INTEGER NOT NULL,
-                payment_option_name TEXT NOT NULL,
-                account_number INTEGER NOT NULL
-            )`);
-            db.run(`CREATE TABLE IF NOT EXISTS orders(
-                order_id INTEGER PRIMARY KEY NOT NULL,
-                order_date TEXT NOT NULL,
-                payment_type INTEGER,
-                buyer_id INTEGER NOT NULL
-            )`);
-            db.run(`CREATE TABLE IF NOT EXISTS products(
-                product_id INTEGER PRIMARY KEY NOT NULL,
-                product_type_id INTEGER NOT NULL,
-                seller_id INTEGER NOT NULL,
-                product_name TEXT NOT NULL,
-                description TEXT NOT NULL,
-                quantity_avail INTEGER NOT NULL,
-                price REAL NOT NULL
-            )`);
-            db.run(`CREATE TABLE IF NOT EXISTS productOrders(
-                order_id INTEGER NOT NULL,
-                product_id INTEGER NOT NULL,
-                line_item_id INTEGER PRIMARY KEY NOT NULL
-            )`);
-            db.run(`CREATE TABLE IF NOT EXISTS productTypes(
-                type_id INTEGER PRIMARY KEY NOT NULL,
-                label TEXT NOT NULL
-            )`, () => {
-                Promise.all([insertUserRows(), insertProductOrderRows(), insertProductTypesRows(), insertProductRows(), insertOrderRows(), insertPaymentsRows()])
-                .then ( () => {
-                    resolve(); //Resolves when tables are ready to be tested -jmr
-                })
-            });
-        });
-    });
-}
-
 //This compartmentalizes JUST the users DB functions to rebuild just that table rather than the whole database. - the ladies
 function buildUsersDB () {
     db.serialize( () => {
@@ -89,6 +28,72 @@ function buildUsersDB () {
                 email TEXT NOT NULL
             )`);
         insertUserRows();
+    })
+}
+
+
+function buildProductOrdersDB () {
+    db.serialize( () => {
+        db.run(`DROP TABLE IF EXISTS productOrders`);
+        db.run(`CREATE TABLE IF NOT EXISTS productOrders(
+            order_id INTEGER NOT NULL,
+            product_id INTEGER NOT NULL,
+            line_item_id INTEGER PRIMARY KEY NOT NULL
+        )`);
+        insertProductOrderRows();
+    })
+}
+
+function buildProductsDB () {
+    db.serialize( () => {
+        db.run(`DROP TABLE IF EXISTS products`);
+        db.run(`CREATE TABLE IF NOT EXISTS products(
+            product_id INTEGER PRIMARY KEY NOT NULL,
+            product_type_id INTEGER NOT NULL,
+            seller_id INTEGER NOT NULL,
+            product_name TEXT NOT NULL,
+            description TEXT NOT NULL,
+            quantity_avail INTEGER NOT NULL,
+            price REAL NOT NULL
+        )`);
+        insertProductRows();
+    })
+}
+
+function buildProductTypeDB () {
+    db.serialize( () => {
+        db.run(`DROP TABLE IF EXISTS productTypes`);
+        db.run(`CREATE TABLE IF NOT EXISTS productTypes(
+            type_id INTEGER PRIMARY KEY NOT NULL,
+            label TEXT NOT NULL
+        )`);
+        insertProductTypesRows();
+    })
+}
+
+function buildPaymentsDB () {
+    db.serialize( () => {
+        db.run(`DROP TABLE IF EXISTS paymentOptions`);
+        db.run(`CREATE TABLE IF NOT EXISTS paymentOptions(
+            payment_id INTEGER PRIMARY KEY NOT NULL,
+            buyer_id INTEGER NOT NULL,
+            payment_option_name TEXT NOT NULL,
+            account_number INTEGER NOT NULL
+        )`);
+        insertPaymentsRows();
+    })
+}
+
+function buildOrdersDB () {
+    db.serialize( () => {
+        db.run(`DROP TABLE IF EXISTS orders`);
+        db.run(`CREATE TABLE IF NOT EXISTS orders(
+            order_id INTEGER PRIMARY KEY NOT NULL,
+            order_date TEXT NOT NULL,
+            payment_type INTEGER,
+            buyer_id INTEGER NOT NULL
+        )`);
+        insertOrderRows();
     })
 }
 
@@ -220,4 +225,11 @@ function insertOrderRows () {
     })
 }
 
-module.exports = { functionThatCreatesTables, buildUsersDB };
+buildUsersDB(); 
+buildOrdersDB();
+buildPaymentsDB();
+buildProductsDB();
+buildProductTypeDB();
+buildProductOrdersDB();
+
+module.exports = { buildUsersDB, buildOrdersDB, buildPaymentsDB, buildProductsDB, buildProductTypeDB, buildProductOrdersDB };
--- test/getuser.test.js
+++ test/getuser.test.js
@@ -2,7 +2,6 @@
 
 const { assert: {equal, property, isFunction, isObject} } = require('chai');
 const { getOneUser, postUserObj } = require('../app/models/Customer.js');
-const { functionThatCreatesTables } = require('../db/build-db.js');
 
 describe('Get Customer', () => {
   it('should be a function', () => isFunction(getOneUser, 'Function?'));
