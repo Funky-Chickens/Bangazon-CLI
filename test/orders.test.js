cb421edb2f3f43fc9f177f50eda6504b336579ac
An-AngryBear <monahajt@gmail.com>
2017-09-28 14:13:03 -0500

    reformat describe blocks in orders test

--- test/orders.test.js
+++ test/orders.test.js
@@ -4,36 +4,32 @@ const { assert: {equal, isFunction, isNull, isNumber, isObject} } = require('cha
 const { checkForOpenOrders, openOrderPost, getOneOrder } = require('../app/models/Order.js');
 const { buildOrdersDB, buildProductOrdersDB } = require('../db/build-db.js');
 
-
-describe('Check for open orders', () => {
-  before( function(done) {
-    buildOrdersDB()
-    .then( () => done()); 
-  });
-    it('should be a function', () => isFunction(checkForOpenOrders, 'Function?'));
-    it('should be a number', () => {
-        return checkForOpenOrders(2)
-        .then( (orderCheckResult) => {
-            isNumber(orderCheckResult);
+describe('Orders', () => {
+    before( function(done) {
+        buildOrdersDB()
+        .then( () => {
+            return buildProductOrdersDB()
+        })
+        .then( () => done()); 
+    });
+    describe('Check for open orders', () => {
+        it('should be a function', () => isFunction(checkForOpenOrders, 'Function?'));
+        it('should be a number', () => {
+            return checkForOpenOrders(2)
+            .then( (orderCheckResult) => {
+                isNumber(orderCheckResult);
+            });
         });
     });
-});
-
-describe('Open Order Post', () => {
-  before( function(done) {
-    buildOrdersDB()
-    .then( () => {
-    return buildProductOrdersDB()
-    })
-    .then( () => done()); 
-  });
-    it('should be a function', () => isFunction(openOrderPost, 'Function?'));
-    it('should have the product that was posted', () => {
-        return openOrderPost(1, 8)
-        .then( (data) => {
-            return getOneOrder(1)
-            .then( (order) => {
-                equal(order[order.length -1].prod_id, 8);
+    describe('Open Order Post', () => {
+        it('should be a function', () => isFunction(openOrderPost, 'Function?'));
+        it('should have the product that was posted', () => {
+            return openOrderPost(1, 8)
+            .then( (data) => {
+                return getOneOrder(1)
+                .then( (order) => {
+                    equal(order[order.length -1].prod_id, 8);
+                });
             });
         });
     });
