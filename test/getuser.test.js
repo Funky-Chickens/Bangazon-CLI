77162d04d7357fa57dd8c783174ff14a2b767705
Greta Moseley <greta.moseley@gmail.com>
2017-09-28 13:35:26 -0500

    "refactor tests to add proper describe wrapper, test for array of objects being returned by get all customers"

--- test/getuser.test.js
+++ test/getuser.test.js
@@ -1,16 +1,27 @@
 'use strict';
 
-const { assert: {equal, property, isFunction, isObject} } = require('chai');
-const { getOneUser, postUserObj } = require('../app/models/Customer.js');
+const { assert: {equal, property, isFunction, isArray, isObject} } = require('chai');
+const { getOneUser, postUserObj, getAllUsers } = require('../app/models/Customer.js');
 
-describe('Get Customer', () => {
-  it('should be a function', () => isFunction(getOneUser, 'Function?'));
-  it('should return an object', () => {
-    getOneUser(1) //call function that returns a resolved result -el/gm
-    .then( (result) => isObject(result));
+describe('Customer', () => {//model
+  describe('Get One Customer', () => {//feature
+    it('should be a function', () => isFunction(getOneUser, 'Function?'));
+    it('should return an object', () => {
+      getOneUser(1) //call function that returns a resolved result -el/gm
+      .then( (result) => isObject(result));
+    });
+    it('should get a user', () => {
+      getOneUser(1)
+      .then( (result) => property(result, 'last_name'));
+    });
   });
-  it('should get a user', () => {
-    getOneUser(1)
-    .then( (result) => property(result, 'last_name'));
-  });
-});
+  describe('GetAllCustomers',() =>{
+    it('should get all userObjects in an array', () =>{
+      return getAllUsers()
+      .then( (allUsers) =>{
+        console.log("results of getAll users", allUsers);
+        isArray(allUsers);
+      })
+    })
+  })
+})
